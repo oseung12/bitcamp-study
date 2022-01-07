61b676c710370b9b3af8993935dae371797d9709
oseung12 <osungg@gmail.com>
2022-01-07 09:40:56 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
@@ -9,11 +9,8 @@ import com.eomcs.util.ArrayList;
 @RestController 
 public class BoardController {
 
+  // Board 객체 목록을 저장할 메모리를 준비한다.
   ArrayList boardList = new ArrayList();
-
-  public BoardController() {
-    System.out.println("BoardController() 호출됨!");
-  }
 
   @RequestMapping("/board/list")
   public Object list() {
