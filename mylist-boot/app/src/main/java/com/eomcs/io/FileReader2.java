1a67c83dde5fd0eb0df37cbfa4a150e2f2ddbd08
oseung12 <osungg@gmail.com>
2022-01-10 17:42:36 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/io/FileReader2.java
+++ mylist-boot/app/src/main/java/com/eomcs/io/FileReader2.java
@@ -1,20 +1,23 @@
 package com.eomcs.io;
 
-// 기존 코드를 자신의 코드인것처럼 사용하겠다고 선언한다.
-public class FileReader2 extends java.io.FileReader {
+import java.io.FileReader;
 
-  // 슈퍼 클래스의 생성자는 바로 사용할 수 없다.
-  // 서브 클래스의 생성자를 통해 사용해야 한다.
+// 기존 코드를 자신의 코드인것처럼 사용하기 위해 해당 클래스를 포함한다.
+public class FileReader2 {
+
+  // 기존의 클래스 포함!
+  FileReader in;
+
   public FileReader2(String filename) throws Exception {
 
-    // 다음과 같이 수퍼 클래스의 생성자를 호출해야 한다.
-    super(filename);
+    // 기존 객체를 준비한다.
+    in = new FileReader(filename);
   }
 
   public String readLine() throws Exception {
     StringBuilder buf = new StringBuilder();
     int c;
-    while ((c = this.read()) != -1) {// 파일에서 한 문자를 읽는다. 더이상 읽을 문자가 없으면 반복문을 종료한다.
+    while ((c = in.read()) != -1) {// 파일에서 한 문자를 읽는다. 더이상 읽을 문자가 없으면 반복문을 종료한다.
       if (c == '\n') { // 만약 읽은 문자가 줄바꿈 명령이라면, 지금까지 버퍼에 저장한 문자를 리턴한다.
         return buf.toString();
       } else if (c == '\r') { 
@@ -24,5 +27,10 @@ public class FileReader2 extends java.io.FileReader {
       }
     }
     return buf.toString();
+  }
+
+  // 자원해제하라는 요청이 들어오면 실제 일을 하는 객체에게 전달한다.
+  public void close() throws Exception {
+    in.close();
   }
 }
