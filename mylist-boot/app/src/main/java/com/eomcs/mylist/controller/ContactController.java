13b6cbb68b6de4c9f3ab969112dac32b72bf622a
oseung12 <osungg@gmail.com>
2022-01-19 14:29:26 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.eomcs.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.domain.Contact;
@@ -19,11 +21,21 @@ public class ContactController {
     contactList = new ArrayList();
     System.out.println("ContactController() 호출됨!");
 
-    BufferedReader in = new BufferedReader(new FileReader("contacts.csv"));
+    DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("contacts.data")));
 
-    String line;
-    while ((line = in.readLine()) != null) {
-      contactList.add(Contact.valueOf(line)); 
+    while (true) {
+      try {
+        Contact contact = new Contact();
+        contact.setName(in.readUTF());
+        contact.setEmail(in.readUTF());
+        contact.setTel(in.readUTF());
+        contact.setCompany(in.readUTF());
+
+        contactList.add(contact);
+
+      } catch (Exception e) {
+        break;
+      }
     }
 
     in.close();
@@ -74,12 +86,15 @@ public class ContactController {
 
   @RequestMapping("/contact/save")
   public Object save() throws Exception {
-    PrintWriter out = new PrintWriter(new FileWriter("contacts.csv")); // 따로 경로를 지정하지 않으면 파일은 프로젝트 폴더에 생성된다.
+    DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("contacts.data"))); 
 
     Object[] arr = contactList.toArray();
     for (Object obj : arr) {
       Contact contact = (Contact) obj;
-      out.println(contact.toCsvString());
+      out.writeUTF(contact.getName());
+      out.writeUTF(contact.getEmail());
+      out.writeUTF(contact.getTel());
+      out.writeUTF(contact.getCompany());
     }
 
     out.close();
--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
@@ -1,9 +1,11 @@
 package com.eomcs.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.domain.Todo;
@@ -18,11 +20,17 @@ public class TodoController {
     todoList = new ArrayList();
     System.out.println("TodoController() 호출됨!");
 
-    BufferedReader in = new BufferedReader(new FileReader("todos.csv"));
+    DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("todos.data")));
 
-    String line;
-    while ((line = in.readLine()) != null) {
-      todoList.add(Todo.valueOf(line)); 
+    while (true) {
+      try {
+        Todo todo = new Todo();
+        todo.setTitle(in.readUTF());
+        todo.setDone(in.readBoolean());
+        todoList.add(todo);
+      } catch (Exception e) {
+        break;
+      }
     }
 
     in.close();
@@ -74,12 +82,13 @@ public class TodoController {
 
   @RequestMapping("/todo/save")
   public Object save() throws Exception {
-    PrintWriter out = new PrintWriter(new FileWriter("todos.csv")); // 따로 경로를 지정하지 않으면 파일은 프로젝트 폴더에 생성된다.
+    DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("todos.data"))); // 따로 경로를 지정하지 않으면 파일은 프로젝트 폴더에 생성된다.
 
     Object[] arr = todoList.toArray();
     for (Object obj : arr) {
       Todo todo = (Todo) obj;
-      out.println(todo.toCsvString());
+      out.writeUTF(todo.getTitle());
+      out.writeBoolean(todo.isDone());
     }
 
     out.close();
