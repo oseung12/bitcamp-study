ee2f77d0a51eabb1a80a78c671250e4a0507743b
oseung12 <osungg@gmail.com>
2022-01-25 21:18:09 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/JsonTodoDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/JsonTodoDao.java
@@ -1,31 +1,32 @@
 package com.eomcs.mylist.dao;
 
 import java.io.File;
-import com.eomcs.mylist.domain.Board;
+import org.springframework.stereotype.Repository;
+import com.eomcs.mylist.domain.Todo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // @Repository
 // - 클래스에 이 애노테이션을 붙여 표시해 두면, Spring Boot가 실행될 때 이 클래스의 객체를 자동 생성한다.
 // - 또한 이 객체를 원하는 곳에 자동으로 주입한다.
 //
-//@Repository  
-public class JsonBoardDao extends AbstractBoardDao {
+@Repository  
+public class JsonTodoDao extends AbstractTodoDao {
 
-  String filename = "boards.json";
+  String filename = "todos.json";
 
-  public JsonBoardDao() {
+  public JsonTodoDao() {
     try {
       ObjectMapper mapper = new ObjectMapper();
-      boardList.addAll(mapper.readValue(new File(filename), Board[].class));
+      todoList.addAll(mapper.readValue(new File(filename), Todo[].class));
 
     } catch (Exception e) {
-      System.out.println("게시글 데이터 로딩 중 오류 발생!");
+      System.out.println("해야 할 일 데이터 로딩 중 오류 발생!");
     }
   }
 
   @Override
   protected void save() throws Exception {
     ObjectMapper mapper = new ObjectMapper();
-    mapper.writeValue(new File(filename), boardList.toArray());
+    mapper.writeValue(new File(filename), todoList.toArray());
   }
 }
