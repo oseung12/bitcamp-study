62b47af9564784b41812373f88e4f360daeb61f8
oseung12 <osungg@gmail.com>
2022-02-02 21:36:34 +0900

    .

---
+++ practic-app1/app/src/main/java/com/eomcs/app1/chapter2/Ex2_2.java
@@ -0,0 +1,11 @@
+package com.eomcs.app1.chapter2;
+
+public class Ex2_2 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+    System.out.printf("3 + 5 = ");
+    System.out.println(3+5);
+  }
+
+}
