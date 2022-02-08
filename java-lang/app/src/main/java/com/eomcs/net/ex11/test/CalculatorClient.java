540ed8529adfcf9b9def0c14bf68cabae4f81387
oseung12 <osungg@gmail.com>
2022-02-08 17:56:53 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorClient.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 
 public class CalculatorClient {
   public static void main(String[] args) {
-    try(
-        Socket socket = new Socket("localhost", 8888);   //try 안에 변수선언이 와야한다.
+    try (
+        Scanner keyScan = new Scanner(System.in);
+        Socket socket = new Socket("localhost", 8888);
         PrintStream out = new PrintStream(socket.getOutputStream());
-        Scanner in = new Scanner (socket.getInputStream());
-        ){
+        Scanner in = new Scanner(socket.getInputStream());
+        ) {
 
       while (true) {
         String str = in.nextLine();
@@ -20,11 +21,26 @@ public class CalculatorClient {
         System.out.println(str);
       }
 
-      out.println("test... ok!");
-      out.flush();
+      while (true) {
+        System.out.print("계산식> ");
+        String input = keyScan.nextLine();
 
-      String str = in.nextLine();
-      System.out.println(str);
+
+        if(!input.equals("quit") && input.split(" ").length !=3) {
+          System.out.println("입력 형식이 올바르지 않습니다. 예) 23 + 5");
+          continue;
+        }
+
+        out.println(input);
+        out.flush();
+
+        String str = in.nextLine();
+        System.out.println(str);
+
+        if (input.equals("quit")) {
+          break;
+        }
+      }
 
     } catch (Exception e) {
       System.out.println("서버 연결 오류!");
--- java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex11/test/CalculatorServer.java
@@ -9,50 +9,101 @@ import java.util.Scanner;
 
 public class CalculatorServer {
 
-  public CalculatorServer() {
+  String logo;
 
+  public CalculatorServer() {
+    StringBuilder logoStr = new StringBuilder();
+
+    try (BufferedReader welcomeIn = new BufferedReader(new FileReader("welcome.txt"));) {
+      while (true) {
+        String str = welcomeIn.readLine();
+        if (str == null) {
+          break;
+        }
+        logoStr.append(str + "\n");
+      }
+    } catch (Exception e) {
+      if (logoStr.length() > 0) {
+        logoStr.setLength(0);
+      }
+      logoStr.append("환영합니다!\n");
+    }
+
+    logo = logoStr.toString();
   }
 
-  public static void main (String[] args) {
-
-    //close를 자동으로 하기위해 ServerSocket serverSocket = new ServerSocket(8888))를 try안에 둔다.
-    try (ServerSocket serverSocket = new ServerSocket(8888)) {  
-      System.out.println("서버 실행 중 ..");
+  public void launch(int port) {
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      System.out.println("서버 실행 중...");
 
       while (true) {
-        try(
-            Socket socket = serverSocket.accept();  // Socket에 담는다.
-            Scanner in = new Scanner(socket.getInputStream());
-            PrintStream out = new PrintStream(socket.getOutputStream());
-            ){
-
-          try (BufferedReader welcomeIn = new BufferedReader(new FileReader("welcome.txt"));) {
-            while (true) {
-              String str = welcomeIn.readLine();
-              if (str == null) {
-                break;
-              }
-              out.println(str);
-            }
-          } catch (Exception e) {
-            out.println("환영합니다!");
-          }
-
-          out.println("계산식을 입력하세요!");
-          out.println("예) 23 + 7 ");
-          out.println();
-
-          String str = in.nextLine();
-          out.println(str);
-          out.flush();
-
-        } catch (Exception e) {
-          System.out.println("클라이언트 요청 처리 중 오류 발생!");
-        }
+        new RequestHandler(serverSocket.accept()).start();
       }
 
     } catch (Exception e) {
       System.out.println("서버 소켓 생성 중 오류 발생!");
     }
   }
+
+  public static void main(String[] args) {
+    new CalculatorServer().launch(8888);
+  }
+
+  class RequestHandler extends Thread {
+    Socket socket;
+
+    public RequestHandler(Socket socket) {
+      this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+      try (
+          Socket socket2 = socket; // close() 자동 호출하기 위해
+          Scanner in = new Scanner(socket.getInputStream());
+          PrintStream out = new PrintStream(socket.getOutputStream());
+          ) {
+
+        out.print(logo);
+
+        out.println("계산식을 입력하세요!");
+        out.println("예) 23 + 7 ");
+        out.println();
+
+        while (true) {
+          String str = in.nextLine();
+          if (str.equals("quit")) {
+            out.println("Goodbye!");
+            out.flush();
+            break;
+          }
+
+          try {
+            String[] values = str.split(" ");
+            int a = Integer.parseInt(values[0]);
+            int b = Integer.parseInt(values[2]);
+            String op = values[1];
+
+            switch (op) {
+              case "+": out.printf("%d %s %d = %d\n", a, op, b , a + b); break;
+              case "-": out.printf("%d %s %d = %d\n", a, op, b , a - b);break;
+              case "*": out.printf("%d %s %d = %d\n", a, op, b , a * b); break;
+              case "/": out.printf("%d %s %d = %d\n", a, op, b , a / b); break;
+              case "%": out.printf("%d %s %d = %d\n", a, op, b , a % b); break;
+              default : out.printf("%d %s %d = %s\n", a, op, b , "지원하지 않는 연산입니다."); 
+            }
+            out.flush();
+
+          } catch (Exception e) {
+            out.println("계산 중 오류 발생 - " + e.getMessage());
+            out.flush();
+          }
+        }
+
+      } catch (Exception e) {
+        System.out.println("클라이언트 요청 처리 중 오류 발생!");
+      }
+    }
+  }
+
 }
