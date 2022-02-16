64bb4a99555653cb7e5ed1224acb754f7b8982b9
oseung12 <osungg@gmail.com>
2022-02-16 12:44:07 +0900

    .

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -1,11 +1,12 @@
-
 package com.eomcs.app2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
   Scanner keyScan = new Scanner(System.in);
+  ArrayList<Score> scores = new ArrayList<>();
 
   public static void main(String[] args) {
     new App().service();
@@ -15,7 +16,7 @@ public class App {
 
     while (true) {
       printMenu();
-      String input = prompt();
+      String input = promptString("명령> ");
 
       if (checkQuit(input)) {
         break;
@@ -32,8 +33,9 @@ public class App {
             System.out.println("올바른 메뉴 번호를 입력하세요!");
         }
       } catch (Exception e) {
-        System.out.println("실행 중 오류 발생 : " + e.getMessage());
+        System.out.println("실행 중 오류 발생: " + e.getMessage());
       }
+
       System.out.println();
     }
 
@@ -50,9 +52,13 @@ public class App {
     System.out.println("5. 삭제");
   }
 
-  private String prompt() {
-    System.out.print("명령> ");
+  private String promptString(String title) {
+    System.out.print(title);
     return keyScan.nextLine();
+  }
+
+  private int promptInt(String title) {
+    return Integer.parseInt(promptString(title));
   }
 
   private boolean checkQuit(String input) {
@@ -60,37 +66,71 @@ public class App {
   }
 
   private void createScore() {
-    System.out.println("이름? ");
-    String name = keyScan.nextLine();
+    Score score = new Score();
+    score.setName(promptString("이름? "));
+    score.setKor(promptInt("국어? "));
+    score.setEng(promptInt("영어? "));
+    score.setMath(promptInt("수학? "));
 
-    System.out.println("국어? ");
-    int kor = Integer.parseInt(keyScan.nextLine());
-
-    System.out.println("영어? ");
-    int eng = Integer.parseInt(keyScan.nextLine());
-
-    System.out.println("수학? ");
-    int math = Integer.parseInt(keyScan.nextLine());
-
+    scores.add(score);
   }
 
   private void listScore() {
-
+    int count = 0;
+    for (Score score : scores) {
+      System.out.printf("%d: %s, %d, %d, %d, %d, %.1f\n",
+          count++,
+          score.getName(), 
+          score.getKor(),
+          score.getEng(),
+          score.getMath(),
+          score.getSum(),
+          score.getAverage());
+    }
   }
 
   private void detailScore() {
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
 
+    Score score = scores.get(no);
+    System.out.printf("이름: %s\n", score.getName());
+    System.out.printf("국어: %d\n", score.getKor());
+    System.out.printf("영어: %d\n", score.getEng());
+    System.out.printf("수학: %d\n", score.getMath());
+    System.out.printf("합계: %d\n", score.getSum());
+    System.out.printf("평균: %.1f\n", score.getAverage());
   }
 
   private void updateScore() {
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
 
+    Score old = scores.get(no);
+
+    Score score = new Score();
+    score.setName(promptString(String.format("이름(%s)? ", old.getName())));
+    score.setKor(promptInt(String.format("국어(%d)? ", old.getKor())));
+    score.setEng(promptInt(String.format("영어(%d)? ", old.getEng())));
+    score.setMath(promptInt(String.format("수학(%d)? ", old.getMath())));
+
+    scores.set(no, score);
   }
 
   private void deleteScore() {
-
+    int no = promptInt("번호? ");
+    if (no < 0 || no >= scores.size()) {
+      System.out.println("번호가 유효하지 않습니다.");
+      return;
+    }
+    scores.remove(no); 
   }
 }
-
-
 
 
