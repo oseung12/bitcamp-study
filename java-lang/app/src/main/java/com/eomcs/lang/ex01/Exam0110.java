78990a55a053f614d5f1d9d0a22f1cf7c2473f76
oseung12 <osungg@gmail.com>
2022-03-06 21:49:37 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/lang/ex01/Exam0110.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex01/Exam0110.java
@@ -7,7 +7,7 @@ package com.eomcs.lang.ex01;
 //
 
 class Exam0110 {}
- 
+
 //## 실습
 //1) 컴파일하기
 //- $ javac -d bin/main -encoding UTF-8 src/main/java/com/eomcs/basic/ex01/Exam0110.java
@@ -15,9 +15,3 @@ class Exam0110 {}
 //2) 생성된 클래스 파일 확인하기
 //- bin/main/com/eomcs/basic/ex01 디렉토리에 Exam0110.class 파일이 생성된다
 //
-
-
-
-
-
-
