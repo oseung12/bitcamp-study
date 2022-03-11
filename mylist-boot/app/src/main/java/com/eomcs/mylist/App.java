a6cbadf7c32ffcb57484a91a5085cfd9cc5c1d1e
oseung12 <osungg@gmail.com>
2022-03-11 13:42:30 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,9 +1,12 @@
 package com.eomcs.mylist;
 
+import javax.sql.DataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class App {
 
+  // Spring 프레임워크(IoC = 객체풀)에서 객체를 생성한 후 보관하도록 만드는 방법
+  @Bean // => 다음 메서드를 호출한 후 이 메서드가 리턴한 값을 스프링 부트에 보관하라고 지시하는 애노테이션
+  public DataSource createDataSource() {
+    System.out.println("createDataSource() 호출됨!!!!");
+    try {
+      DriverManagerDataSource connectionPool = new DriverManagerDataSource();
+      connectionPool.setDriverClassName("org.mariadb.jdbc.Driver");
+      connectionPool.setUrl("jdbc:mariadb://localhost:3306/studydb");
+      connectionPool.setUsername("study");
+      connectionPool.setPassword("1111");
+
+      return connectionPool;
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);  //스프링부트 실행
+    SpringApplication.run(App.class, args);
   }
 
   //@Bean
