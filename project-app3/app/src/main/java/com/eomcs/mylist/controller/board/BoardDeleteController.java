fac6127a0be4305e70e8144caaa698c080425fc1
oseung12 <osungg@gmail.com>
2022-04-20 09:46:22 +0900

    POJO

--- project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDeleteController.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDeleteController.java
@@ -1,46 +1,32 @@
 package com.eomcs.mylist.controller.board;
 
-import java.io.IOException;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.mylist.controller.Component;
+import com.eomcs.mylist.controller.Controller;
 import com.eomcs.mylist.domain.Board;
 import com.eomcs.mylist.domain.Member;
 import com.eomcs.mylist.service.BoardService;
 
-@SuppressWarnings("serial")
-@WebServlet("/board/delete") 
-public class BoardDeleteController extends HttpServlet {
+@Component("/board/delete")
+public class BoardDeleteController implements Controller {
 
   BoardService boardService;
 
-  @Override
-  public void init() throws ServletException {
-    ServletContext 웹애플리케이션보관소 = this.getServletContext();
-    boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
+  public BoardDeleteController(BoardService boardService) {
+    this.boardService = boardService;
   }
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    Board board = new Board();
+    board.setNo(Integer.parseInt(request.getParameter("no")));
 
-    try {
-      Board board = new Board();
-      board.setNo(Integer.parseInt(request.getParameter("no")));
+    Member loginUser = (Member) request.getSession().getAttribute("loginUser");
+    board.setWriter(loginUser);
 
-      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-      board.setWriter(loginUser);
+    boardService.delete(board);
 
-      boardService.delete(board);
-
-      response.sendRedirect("list");
-
-    } catch (Exception e) {
-      request.setAttribute("exception", e);
-      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
-    }
+    return "redirect:list";
   }
 }
--- project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDetailController.java
+++ project-app3/app/src/main/java/com/eomcs/mylist/controller/board/BoardDetailController.java
@@ -1,40 +1,32 @@
 package com.eomcs.mylist.controller.board;
 
-import java.io.IOException;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.eomcs.mylist.controller.Component;
+import com.eomcs.mylist.controller.Controller;
 import com.eomcs.mylist.domain.Board;
 import com.eomcs.mylist.service.BoardService;
 
-@SuppressWarnings("serial")
-@WebServlet("/board/detail") 
-public class BoardDetailController extends HttpServlet {
+@Component("/board/detail")
+public class BoardDetailController implements Controller {
 
   BoardService boardService;
 
-  @Override
-  public void init() throws ServletException {
-    ServletContext 웹애플리케이션보관소 = this.getServletContext();
-    boardService = (BoardService) 웹애플리케이션보관소.getAttribute("boardService");
+  public BoardDetailController(BoardService boardService) {
+    this.boardService = boardService;
   }
 
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
-      Board board = boardService.get(no);
-      request.setAttribute("board", board);
-      request.getRequestDispatcher("/jsp/board/detail.jsp").forward(request, response);
-
-    } catch (Exception e) {
-      request.setAttribute("exception", e);
-      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
-    }
+  public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    int no = Integer.parseInt(request.getParameter("no"));
+    Board board = boardService.get(no);
+    request.setAttribute("board", board);
+    return "/jsp/board/detail.jsp";
   }
 }
+
+
+
+
+
+
